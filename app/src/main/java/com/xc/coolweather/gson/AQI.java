fab747879362bc2d2e1c4c071b7b7a72232a6302
githubxc <softwarexc@163.com>
2017-06-08 15:21:39 +0800

    显示天气信息。

---
+++ app/src/main/java/com/xc/coolweather/gson/AQI.java
@@ -0,0 +1,14 @@
+package com.xc.coolweather.gson;
+
+/**
+ * Created by XC on 2017/6/7.
+ */
+
+public class AQI {
+
+    public AQICity city;
+    public class  AQICity{
+        public String aqi;
+        public String pm25;
+    }
+}
---
+++ app/src/main/java/com/xc/coolweather/gson/Now.java
@@ -0,0 +1,20 @@
+package com.xc.coolweather.gson;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by XC on 2017/6/7.
+ */
+
+public class Now {
+    @SerializedName("tmp")
+    public String temperature;
+
+    @SerializedName("cond")
+    public More more;
+
+    public class More{
+        @SerializedName("txt")
+        public String info;
+    }
+}
